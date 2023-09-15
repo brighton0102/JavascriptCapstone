517e3d74eeee3b5460c8ce82c467a2fad7733aa5
Karan Joshi <Karan190213@gmail.com>
2023-09-15 19:38:14 +0530

    Linter erro fix

--- src/index.js
+++ src/index.js
@@ -6,15 +6,15 @@ import reservationCounter from './modules/counterModule.js';
 fetchAndRenderData();
 
 function updateReservationCount() {
-    const reserveContainer = document.querySelector('.reservation-container');
-    const reservationCountElement = document.querySelector('.reservation-count');
-  
-    if (reserveContainer && reservationCountElement) {
-      const count = reservationCounter(reserveContainer);
-      reservationCountElement.textContent = count;
-    } else {
-      // Handle the case when the elements are not found, e.g., show an error message
-      console.error('Elements not found');
-    }
+  const reserveContainer = document.querySelector('.reservation-container');
+  const reservationCountElement = document.querySelector('.reservation-count');
+
+  if (reserveContainer && reservationCountElement) {
+    const count = reservationCounter(reserveContainer);
+    reservationCountElement.textContent = count;
+  } else {
+    console.error('Elements not found');
   }
-  
+}
+
+window.addEventListener('load', updateReservationCount);
